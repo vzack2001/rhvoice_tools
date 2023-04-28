b7cbe432f880f5631f1e26ca93d2f7ff2b9b7909
vzack2001 <47769435+vzack2001@users.noreply.github.com>
2023-04-28 14:22:07 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='preprocessing',
+    version='2.0.0',
+    author='Ivan vantu5z',
+    author_email='vantu5z@mail.ru',
+    url='https://github.com/vzack2001/rhvoice_tools',
+    description='plural forms',
+    license='MIT',
+    packages=find_packages(),
+    long_description='RHVoice-dictionary-preprocessing-tools',
+)
